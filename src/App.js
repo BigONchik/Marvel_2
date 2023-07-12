6cb39f61ff212b20a7c8bb5f39fbd561f668b981
BigONchik <128064684+BigONchik@users.noreply.github.com>
2023-07-12 19:12:04 +0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+import MainRoutes from "./routes/MainRoutes";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <MainRoutes />
+    </div>
+  );
+};
+
+export default App;
